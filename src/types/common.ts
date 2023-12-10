73ea25c93b4fd5c4e216125d5c5d43dc8992fbd8
Christoph Gerle <jey-cee@live.com>
2023-12-10 13:47:41 +0100

    Update common.ts

--- src/types/common.ts
+++ src/types/common.ts
@@ -8,8 +8,8 @@ export type DeviceStatus =
            * This can either be the name of a font awesome icon (e.g. "fa-signal") or the URL to an icon.
            */
           icon?: string;
-          // @ts-expect-error
-          battery?: number | boolean| "charging" | string & Record<string, never>; // in percent (0-100), or string 'charging',
+          // eslint-disable-next-line @typescript-eslint/ban-types
+          battery?: number | boolean| "charging" | string; // in percent (0-100), or string 'charging',
                                                                 // or string '10V',
                                                                 // or string '10mV',
                                                                 // or string '100' in mV
