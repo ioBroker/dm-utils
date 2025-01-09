bac9387666d3c31769924fb25d53b3b4bbc3b6cb
GermanBluefox <dogafox@gmail.com>
2025-01-09 22:09:03 +0000

    Changed description of reserved actions

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
 export type * from './adapter';
 export type * from './common';
 export * from './errorCodes';
-export type { ChannelInfo, Color, ControlState, ControlBase } from './base';
+export { type ChannelInfo, type Color, type ControlState, type ControlBase, ACTIONS } from './base';
