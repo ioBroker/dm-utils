47e07e5425b3a2b3829af8c70e2a6cc23ac9686d
Samuel Weibel <samuel.weibel@gmail.com>
2026-02-18 15:54:55 +0000

    Add response types to the API exports

--- src/types/api.ts
+++ src/types/api.ts
@@ -6,6 +6,8 @@ export type DeviceAction = base.DeviceAction<'api'>;
 export type InstanceDetails = base.InstanceDetails<'api'>;
 export type DeviceInfo = base.DeviceInfo<'api'>;
 export type DeviceControl = base.DeviceControl<'api'>;
+export type DeviceRefreshResponse = base.DeviceRefreshResponse<'api'>;
+export type InstanceRefreshResponse = base.InstanceRefreshResponse;
 
 export type DeviceLoadIncrement = {
     add: DeviceInfo[];
