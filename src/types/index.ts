c326310948acc1544264396c403923f40d8f8e7f
GermanBluefox <dogafox@gmail.com>
2026-02-25 11:38:33 +0100

    Added required types export

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,15 @@
 export type * from './adapter';
-export { ACTIONS, type ChannelInfo, type Color, type ControlBase, type ControlState } from './base';
+export {
+    ACTIONS,
+    type ChannelInfo,
+    type Color,
+    type ControlBase,
+    type ControlState,
+    type BackendToGuiCommand,
+    type BackendToGuiCommandAllUpdate,
+    type BackendToGuiCommandDeviceDelete,
+    type BackendToGuiCommandDeviceStatusUpdate,
+    type BackendToGuiCommandDeviceInfoUpdate,
+} from './base';
 export type * from './common';
 export * from './errorCodes';
